c43a99ea90b6b74dfdca5cf6328ddad1c7304bce
Daniel Kertesz <daniel@spatof.org>
2014-04-06 12:30:17 +0200

    cleaner command

--- pasticcio/main.py
+++ pasticcio/main.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 import os
-from optparse import OptionParser
+import argparse
 from hashids import Hashids
 from .app import app, db
+from .cleaner import cleaner
 
 from . import model
 from . import views
@@ -18,14 +19,29 @@ def create_app(config=None):
 
     return app
 
-def main():
-    parser = OptionParser()
-    parser.add_option('-c', '--config')
-    (opts, args) = parser.parse_args()
+def run_server(args):
+    create_app().run()
 
-    if not opts.config and not 'APP_CONFIG' in os.environ:
+def run_cleaner(args):
+    create_app()
+    cleaner(args)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help="Specify a configuration file")
+
+    subp = parser.add_subparsers()
+    cmd_clean = subp.add_parser('cleaner', help="Delete expired pastes")
+    cmd_clean.set_defaults(func=run_cleaner)
+
+    cmd_server = subp.add_parser('server', help="Run development server")
+    cmd_server.set_defaults(func=run_server)
+
+    args = parser.parse_args()
+
+    if not args.config and not 'APP_CONFIG' in os.environ:
         parser.error("You must specify a configuration file")
     else:
-        os.environ['APP_CONFIG'] = os.path.abspath(opts.config)
+        os.environ['APP_CONFIG'] = os.path.abspath(args.config)
 
-    create_app().run()
+    args.func(args)
